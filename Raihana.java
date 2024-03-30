0b3833d5675b07a3e1f3096d78ef5abdb7672351
Raihana-2964 <zainabraihana1985@gmail.com>
2024-03-30 13:13:31 +0530

    updating raihanajava

--- Raihana.java
+++ Raihana.java
@@ -3,11 +3,11 @@ class Main
  public static void main(String[] args)
   {
     
-   int first = 10;
+   int first = 50;
     int second = 20;
 
     // add two numbers
-    int sum = first + second;
+    int sum = first * second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
 }
